a41eeb123d2746cb65ec226188970cb6ad397143
Dhruv Doshi <work@doshidhruv.com>
2025-06-07 10:40:33 -0400

    feat: initialize analyzers module with placeholder imports and exports

---
+++ src/ecoguard_ai/analyzers/__init__.py
@@ -0,0 +1,14 @@
+"""Analyzers module initialization."""
+
+# Import statements will be added as analyzers are implemented
+# from ecoguard_ai.analyzers.quality import QualityAnalyzer
+# from ecoguard_ai.analyzers.security import SecurityAnalyzer  
+# from ecoguard_ai.analyzers.green import GreenAnalyzer
+# from ecoguard_ai.analyzers.ai_code import AICodeAnalyzer
+
+__all__ = [
+    # "QualityAnalyzer",
+    # "SecurityAnalyzer",
+    # "GreenAnalyzer", 
+    # "AICodeAnalyzer",
+]
